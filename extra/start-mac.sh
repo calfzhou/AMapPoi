8cb8ac6a4def8ef6d3f0f616e98f411155a91fbc
Ji Zhou <calf.zhou@gmail.com>
2023-06-09 16:58:17 +0800

    chore: refine mac start script - can run the script from any working directory - adapt different java version

--- extra/start-mac.sh
+++ extra/start-mac.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-java --module-path lib --add-modules javafx.controls,javafx.fxml,javafx.base --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar spider.jar
+program=$0
+# $0 might be a softlink, resolve it.
+while [ "$(readlink -n "$program")" != "" ]; do
+    program=$(readlink -n "$program")
+done
+working_dir=$(dirname "$program")
+cd "$working_dir"
+
+java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+if [[ "$java_version" > "1.8" ]]; then
+    java --module-path lib --add-modules javafx.controls,javafx.fxml,javafx.base --add-exports javafx.base/com.sun.javafx.collections=ALL-UNNAMED -jar spider.jar
+else
+    java -jar spider.jar
+fi
